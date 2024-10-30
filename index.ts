862f2ce6355125548fc56b248598ed3614bdcf0e
Steve <stevedsimkins@gmail.com>
2024-10-29 21:36:08 -0700

    updated script

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 const PINATA_JWT = "";
-const FOLDER_NAME = "";
-import { readdir } from "node:fs/promises";
 import { PinataSDK } from "pinata-web3";
 import { appendFile } from "node:fs/promises";
 
@@ -10,13 +8,14 @@ const pinata = new PinataSDK({
 
 async function main() {
 	try {
-		const files = await readdir(`./${FOLDER_NAME}`);
-		console.log(files);
-		for (const file of files) {
-			const currentFile = Bun.file(`${FOLDER_NAME}/${file}`) as unknown as File;
-			const { IpfsHash } = await pinata.upload.file(currentFile);
-			await appendFile("ipfs-hashes.csv", `${IpfsHash},${file}\n`);
-			console.log("File %s uploaded", file);
+		for (let i = 1; i < 787; i++) {
+      const fileName = `NodeifiMembership-${i}.gif`
+			const currentFile = Bun.file(fileName) as unknown as File;
+			const { IpfsHash } = await pinata.upload.file(currentFile).addMetadata({
+        name: fileName
+      }).group("832c7190-2bdb-4fb4-879d-5bb8e20f357f")
+			await appendFile("ipfs-hashes.csv", `${IpfsHash},${fileName}\n`);
+			console.log("File %s uploaded", fileName)
 		}
 	} catch (error) {
 		console.log(error);
